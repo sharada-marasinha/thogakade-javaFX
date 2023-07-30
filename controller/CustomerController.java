b4434bc50a9b0340dc2cbc0a071a1a7b1d225607
sharada-marasinha <sharadamarasinha@gmail.com>
2023-07-30 23:04:45 +0530

    add form done :)

--- controller/CustomerController.java
+++ controller/CustomerController.java
@@ -1,4 +1,52 @@
 package controller;
 
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import model.Customer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CustomerController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void btnCancelAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddAction(ActionEvent actionEvent) {
+        System.out.println("Hello");
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary= Double.parseDouble(txtSalary.getText());
+        Customer customer = new Customer(id,name,address,salary);
+
+        String SQL = "Insert into Customer Values(?,?,?,?)";
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement psTm = connection.prepareStatement(SQL);
+            psTm.setObject(1,customer.getId());
+            psTm.setObject(2,customer.getName());
+            psTm.setObject(3,customer.getAddress());
+            psTm.setObject(4,customer.getSalary());
+            int i = psTm.executeUpdate();
+            if(i>0){
+                System.out.println("Add sucsss");
+            }else{
+                System.out.println("fall");
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- db/DBConnection.java
+++ db/DBConnection.java
@@ -1,11 +1,20 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
     private static DBConnection instance;
-    private DBConnection(){
-
+    private Connection connection;
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost/Thogakade","root","1234");
     }
-    public static DBConnection getInstance(){
+    public Connection getConnection(){
+        return connection;
+    }
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
         if(null==instance){
             instance=new DBConnection();
         }
